84488071529613ae3af8afb803ccf54865504c14
Artur <artur_goncharov_97@mail.ru>
2022-07-15 23:04:26 +0300

    added calculation of distance for restaurants

--- restaurateur/views.py
+++ restaurateur/views.py
@@ -1,5 +1,7 @@
-from zoneinfo import available_timezones
+import requests
+from geopy import distance
 from django import forms
+from django.conf import settings
 from django.shortcuts import redirect, render
 from django.views import View
 from django.urls import reverse_lazy
@@ -100,9 +102,8 @@ def view_restaurants(request):
 @user_passes_test(is_manager, login_url='restaurateur:login')
 def view_orders(request):
     order_items = OrderItem.objects.select_related("product")
-    pending_orders = Order.custom_manager.prefetch_related(Prefetch('items', queryset=order_items)).summa().order_by('id').filter(status="Необработанный")
+    pending_orders = Order.custom_manager.prefetch_related(Prefetch('items', queryset=order_items)).prefetch_related('restaurant').summa().order_by('id').filter(status="Необработанный")
     restaurants_menu = RestaurantMenuItem.objects.select_related('product').select_related('restaurant')
-    available_restaurants = []
     for pending_order in pending_orders:
         available_restaurants_in_order = []
         pending_order_items = pending_order.items.all()
@@ -113,12 +114,39 @@ def view_orders(request):
         if len(available_restaurants_in_order)>1:
             for restaurants in range(len(available_restaurants_in_order)-1):
                 available_restaurants_in_order[restaurants] = list(set(available_restaurants_in_order[restaurants]) & set(available_restaurants_in_order[restaurants+1]))
-        pending_order.available_restaurants = ''.join(available_restaurants_in_order[0])
+        pending_order = calculation_of_distance_restaurants(pending_order, available_restaurants_in_order[0])
+        print(pending_order.available_restaurants)
     return render(request, template_name='order_items.html', context={
         'orders': pending_orders,
-        'available_restaurants': available_restaurants
     })
 
-# for pending_order in pending_orders:
-#         pending_order_items = pending_order.items.select_related('product')
-#         pending_order_producs = [pending_order_item.product for pending_order_item in pending_order_items]
+
+def calculation_of_distance_restaurants(order, available_restaurants_in_order):
+    deleverey_coordinates = fetch_coordinates(settings.YANDEX_GEOCODER_TOKEN, order.address)
+    available_restaurants = []
+    if len(available_restaurants_in_order) > 0:
+        for restaurant in available_restaurants_in_order:
+            restaurans_coordinates = fetch_coordinates(settings.YANDEX_GEOCODER_TOKEN, restaurant)
+            distance_restaurants = distance.distance(restaurans_coordinates, deleverey_coordinates)
+            restaurant = f"{restaurant} {distance_restaurants}"
+            available_restaurants.append(restaurant)
+    order.available_restaurants = ' '.join(available_restaurants)
+    return order
+
+
+def fetch_coordinates(apikey, address):
+    base_url = "https://geocode-maps.yandex.ru/1.x"
+    response = requests.get(base_url, params={
+        "geocode": address,
+        "apikey": apikey,
+        "format": "json",
+    })
+    response.raise_for_status()
+    found_places = response.json()['response']['GeoObjectCollection']['featureMember']
+
+    if not found_places:
+        return None
+
+    most_relevant = found_places[0]
+    lon, lat = most_relevant['GeoObject']['Point']['pos'].split(" ")
+    return lon, lat
