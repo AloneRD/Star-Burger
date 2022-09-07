2939266fe34cca38e87b215428a84140bddf8168
Artur <artur_goncharov_97@mail.ru>
2022-09-07 21:56:58 +0300

    change db

--- star_burger/settings.py
+++ star_burger/settings.py
@@ -15,7 +15,6 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 SECRET_KEY = env('SECRET_KEY')
 DEBUG = env.bool('DEBUG', True)
 YANDEX_GEOCODER_TOKEN = env('YANDEX_GEOCODER_TOKEN')
-print(DEBUG)
 
 ALLOWED_HOSTS = env.list('ALLOWED_HOSTS', ['127.0.0.1', 'localhost'])
 
@@ -86,10 +85,9 @@ WSGI_APPLICATION = 'star_burger.wsgi.application'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 
+DATABASE_URL = env('DB_URL')
 DATABASES = {
-    'default': dj_database_url.config(
-        default='sqlite:////{0}'.format(os.path.join(BASE_DIR, 'db.sqlite3'))
-    )
+    'default': dj_database_url.config(default=DATABASE_URL)
 }
 
 AUTH_PASSWORD_VALIDATORS = [
